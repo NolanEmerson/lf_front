db00921e21bfbf561ab0f1c967421aa4eafd1515
Nolan <nemerson17@gmail.com>
2018-06-29 14:03:11 -0700

    Completed all basic js functionality

--- main.js
+++ main.js
@@ -13,32 +13,56 @@ class Carousel {
         this.slides = $('.carouselItem');
         this.totalSlides = this.slides.length - 1;
         this.currentSlide = 0;
+        this.beginScroll = null;
 
         this.nextSlide = this.nextSlide.bind(this);
-
+        this.prevSlide = this.prevSlide.bind(this);
+        this.selectSlide = this.selectSlide.bind(this);
+        this.resetScroll = this.resetScroll.bind(this);
+        this.addEventHandlers = this.addEventHandlers.bind(this);
+        
+        this.addEventHandlers();
+        this.createIndicators(this.slides.length);
         this.selectSlide(this.currentSlide);
+    }
 
-        this.beginScroll = setInterval(this.nextSlide, 5000);
+    addEventHandlers() {
+        $('.right').on('click', () => {
+            this.nextSlide();
+        });
+    
+        $('.left').on('click', () => {
+            this.prevSlide();
+        });
+
+        $('.carouselHolder').mouseenter( () => {
+            clearInterval(this.beginScroll);
+        });
+
+        $('.carouselHolder').mouseleave( () => {
+            this.beginScroll = setInterval(this.nextSlide, 2000);
+        });
     }
 
     selectSlide(current) {
-        if (current >=0 && current <= this.totalSlides) {
-            for (let i = 0; i <= this.totalSlides; i++) {
-                if (i === current) {
-                    this.slides[i].style.display = 'inline-block';
-                } else {
-                    this.slides[i].style.display = 'none';
-                }
+        for (let i = 0; i <= this.totalSlides; i++) {
+            if (i === current) {
+                this.slides[i].style.display = 'inline-block';
+            } else {
+                this.slides[i].style.display = 'none';
             }
-        } else {
-            alert('Index not valid number. You shouldn\'t get this issue');
         }
+
+        this.resetScroll();
+        this.colorActiveIndicator(this.currentSlide);
     }
 
     resetScroll() {
         clearInterval(this.beginScroll);
 
-        this.beginScroll();
+        if (!$('.carouselHolder').is(':hover')) {
+            this.beginScroll = setInterval(this.nextSlide, 2000);
+        }
     }
 
     nextSlide() {
@@ -51,4 +75,37 @@ class Carousel {
         this.selectSlide(this.currentSlide);
     }
 
+    prevSlide() {
+        if (this.currentSlide === 0) {
+            this.currentSlide = this.totalSlides;
+        } else {
+            this.currentSlide--;
+        }
+
+        this.selectSlide(this.currentSlide);
+    }
+
+    goToSlide(clicked) {
+        this.currentSlide = clicked;
+        this.selectSlide(clicked);
+    }
+
+    createIndicators(number) {
+        for (let i = 0; i < number; i++) {
+            const newIndicator = $('<div>').addClass('indicator').on('click', () => {
+                this.goToSlide(i);
+            });
+            $('.indicatorHolder').append(newIndicator);
+        }
+    }
+
+    colorActiveIndicator(current) {
+        for (let i = 0; i <= this.totalSlides; i++) {
+            if (i === current) {
+                $('.indicator')[i].style['background-color'] = '#f2f2f2';
+            } else {
+                $('.indicator')[i].style['background-color'] = '';
+            }
+        }
+    }
 }
