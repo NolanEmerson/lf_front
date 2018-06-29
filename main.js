a614c5dab4c03fb34bb4b85440da83ab597ab4e5
Nolan <nemerson17@gmail.com>
2018-06-29 12:43:36 -0700

    Reformatted slides and added auto scrolling

--- main.js
+++ main.js
@@ -10,12 +10,15 @@ class Carousel {
 
     constructor(containerID) {
         this.container = $(`#${containerID}`) || document.body;
-        this.slides = $('.carouselImage');
+        this.slides = $('.carouselItem');
         this.totalSlides = this.slides.length - 1;
         this.currentSlide = 0;
 
+        this.nextSlide = this.nextSlide.bind(this);
+
         this.selectSlide(this.currentSlide);
 
+        this.beginScroll = setInterval(this.nextSlide, 5000);
     }
 
     selectSlide(current) {
@@ -31,4 +34,21 @@ class Carousel {
             alert('Index not valid number. You shouldn\'t get this issue');
         }
     }
+
+    resetScroll() {
+        clearInterval(this.beginScroll);
+
+        this.beginScroll();
+    }
+
+    nextSlide() {
+        if (this.currentSlide === this.totalSlides) {
+            this.currentSlide = 0;
+        } else {
+            this.currentSlide++;
+        }
+
+        this.selectSlide(this.currentSlide);
+    }
+
 }
